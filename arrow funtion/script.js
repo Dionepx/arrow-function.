6b0567241a8288c76b3c4baffc0378527ca8fb1c
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-05-07 23:45:49 -0300

    Add files via upload

---
+++ arrow funtion/script.js
@@ -0,0 +1,18 @@
+function soma(x, y) {
+     return x + y
+}
+
+const soma1 = soma(2, 3)
+
+const multiplicacao = function (x, y) {
+     return x * y
+}
+
+const mult_1 = multiplicacao(3, 2)
+console.log(mult_1)
+
+const dividir = (x, y) => {
+     return x / y
+}
+
+const objeto = () => ({nome: "Dione", sobrenome: "Silva"});
